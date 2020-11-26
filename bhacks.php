8dd5274a6c17d2fc7ce58fbb95cf9b3147b04528
hackstrreetguy <goyalmudit2908@gmail.com>
2020-11-26 11:17:24 +0530

    sponsors update

--- bhacks.php
+++ bhacks.php
@@ -11,7 +11,7 @@
 		<div class="row">
 			<div class="col-md-12 section-heading text-left">
 				<h2 class=" to-animate" style="text-align : center"><img src="images/bhacksll.svg" class="main-logo to-animate" style="height : 550px" width="35%"></h2>
-				<h1 style="color : #fff ;text-align :center">4-6 Dec 2020</h1>
+				<!-- <h1 style="color : #fff ;text-align :center">4-6 Dec 2020</h1> -->
 			</div>
 
 		</div>
@@ -23,14 +23,15 @@
 
 		<h3 style="text-align :center">B-Hacks is a business hackathon organized by the IIITA Info Communication and Incubation Center (IIIC)
 			with the intention of bringing out the best ideas that can be worked upon and can be transformed into a business which generates revenue.
-			We challenge the youth of today to solve the world’s socio-economic problems as well as make money,
-			through the creation of for-profit for-good businesses .
 			<br><br>
 			This year, IIIC is elated to introduce HULT PRIZE, IIITA Chapter which is the world’s largest social entrepreneurship competition.
 			The winners of the Hult Prize Global finals receive prize money of $1,000,000 as startup capital.
-			This program is integrated with B-Hacks as its primary challenge.2020 will mark the second consecutive year that B-Hacks will be held.
 			In the past we have seen participation of more than 150+ candidates, and we expect this number to go up even further this year.
 
+			<br><br>
+			B-Hacks 2.0: A month-long startup challenge starting from 8th Dec 2020 to 8th Jan 2021.
+			This Business hackathon aims to challenge students to make profitable business startups out of the hackathon products/services which can attract customers
+			and potentially generate revenue.
 		</h3>
 		<br>
 		<br>
@@ -40,14 +41,28 @@
 
 		<ul>
 			<li>
-				<h3>The Primary Track(HULT Prize Track)</h3>
+				<h3>Track-1: Hult Prize 2021 Challenge - Food For Good (8-10 Dec)
+				</h3>
 			</li>
-			<p>HULT PRIZE 2021 Challenge - “Food for Good”.<br>
-				Choosing this track will make you participate in the HULT Prize 2021.
-				This track will be focused more on the business plan and pitching in the final round rather than the overall development of the prototype.
-				Winning Teams will further proceed to HULT Regionals.
+			<!-- <p>HULT PRIZE 2021 Challenge - “Food for Good”<br> -->
+			Choosing this track will make you participate in the HULT Prize 2021.
+			This track will be focused more on the business plan and pitching in the final round rather than the overall development of the prototype.
+			Winning Teams will further proceed to HULT Regionals.
 			</p>
-			To know more details about Hult Prize 2021 Challenge click <a href=" uploads/hultprize.pdf" style="color : #fff ; "><u>here</u></a>
+			<center><iframe width="560" height="315" src="https://www.youtube.com/embed/liStv7aHfiE" frameborder="0" allow="accelerometer; 
+			autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+			</center>
+			<br>
+			Please Go through the required details
+			<ul>
+				<li>Details about Hult Prize 2021 Challenge - <a href=" uploads/hultprize.pdf" style="color : #fff ; "><u>here</u></a></li>
+				<li>Hult Prize 2021 Scorecard - <a href=" uploads/scorecard.pdf" style="color : #fff ; "><u>here</u></a></li>
+				<li>Problem Statement Breakdown and Guidelines for Track-1 in B-Hacks - <a href=" uploads/track1_domain.pdf" style="color : #fff ; "><u>here</u></a></li>
+				<li>Student Training Pitch Deck Materials - <a href="https://drive.google.com/drive/folders/1czGEEVUmLLWkCm5Qxt_y4cDdorqd3Lq9" style="color : #fff ; "><u>here</u></a> (Includes PPT and Video ) </li>
+
+			</ul>
+			<br>
+			<!-- To know more details about Hult Prize 2021 Challenge click <a href=" uploads/hultprize.pdf" style="color : #fff ; "><u>here</u></a> -->
 			<p>To Participate in the above track register below you are required to fill the google form
 				<a href="https://docs.google.com/forms/d/e/1FAIpQLSfxgjibwj-aRVC4QIv9U4ovBHFBVpmGhFP2uq0HFu8-ZpRQ6Q/viewform" style="color : #fff ; "><u>here</u></a>
 				as well as register on the link below
@@ -57,10 +72,19 @@
 				<li>
 					<h3>The Secondary Track (NewGen IEDC Track) </h3>
 				</li>
-				<p>This track is not focused on ideating a solution on the primary problem statement.
+				<p>This track is not focused on ideating a solution on some particular problem statement.
 					However, Participants can develop any product/service which can thrive as a revenue generating business while keeping in mind
 					that the idea must be unique in terms of the solution it provides abiding by the core purpose of this hackathon.
-					The best ideas will be selected for the funding opportunities in NewGen IEDC center at IIIT Allahabad.</p>
+					The best ideas will be selected for the funding opportunities in NewGen IEDC center at IIIT Allahabad.
+				</p>
+
+				Please Go through the required details
+				<ul>
+					<li>Problem Statements - <a href=" uploads/track2_domain.pdf" style="color : #fff ; "><u>here</u></a></li>
+					<li>Timeline and Guidelines - <a href=" uploads/track2_guidelines.pdf" style="color : #fff ; "><u>here</u></a></li>
+
+				</ul>
+				<br>
 				<p>To participate in this track register below
 
 					<center><a class="btn btn-rounded btn-primary" href="https://docs.google.com/forms/d/e/1FAIpQLSehLaYJChy--LQ2hjqVsp_0HqB5xBShOP28If3cP9AqzmFa8w/viewform">Register</a></center>
@@ -68,9 +92,18 @@
 		</ul>
 
 		<br><br>
-		<h1 style="color : #fff ;text-align :center">SPONSOR US </h1>
-		<h4 style="color : #fff ;text-align :center"><a style="color : #fff ;text-align :center" href="uploads/sponsorship_brochure.pdf"><u>Download Brochure</u></a></h1>
+		<h1 style="color : #fff ;text-align :center">SPONSORS</h1>
+		<h4 style="color : #fff ;text-align :center"><a style="color : #fff ;text-align :center" href="uploads/brochure_bhacks20.pdf"><u>Download Brochure</u></a></h1>
 			<br>
+			<center>
+				<a href="https://balsamiq.com/"><img src="images/brands/balsamiq.png" width="35%" style="vertical-align:middle; margin:10px 50px 25px 60px"></a>
+				<a href="https://wolfram.com/"><img src="images/brands/wolfram.png" width="35%" style="vertical-align:middle; margin:10px 25px 25px 50px"></a>
+			</center>
+			<br>
+			<center>
+				<a href="https://codingblocks.com/"><img src="images/brands/codingblocks.jpg" width="35%" style="vertical-align:middle; margin:25px 50px 5px 60px"></a>
+				<a href="https://walmart.com/"><img src="images/brands/walmart.jpg" width="35%" style="vertical-align:middle; margin:25px 25px 5px 50px"></a>
+			</center>
 			<h2 style="color : #fff ;text-align :center; margin-top: 30px">PAST SPONSORS</h2>
 			<img src="images/pastsponsors.jpg" width="100%" style="vertical-align:middle; margin:0px 0px"><br>
 			<br><br>
